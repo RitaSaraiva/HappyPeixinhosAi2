fb524f2de0a84e585f7773a86fba8f20cbdce93e
RitaSaraiva <belgas2000@gmail.com>
2021-05-18 18:29:35 +0100

    Persue movemnt and Wall COllider working

--- HappyPeixinhosIA2/Assets/scprits/AIBehaviour.cs
+++ HappyPeixinhosIA2/Assets/scprits/AIBehaviour.cs
@@ -5,15 +5,17 @@ using AIUnityExamples.Movement.Core;
 
 public class AIBehaviour : MonoBehaviour
 {
-    //[SerializeField] private GameArea gameArea;
-    //[SerializeField] private float maxWallDist;
-    
-    [SerializeField] private float wandertime;
-    [SerializeField] private float movementSpeed;
+    [SerializeField] private float wanderTime;
 
-    // Player in sight distance
+    private bool hitWall;
+    private Vector3 targetEulerAngles;
+
+    // Speed of AI agent movement
     [SerializeField]
-    private float playerInSightDistance = 10f;
+    private float movementSpeed;
+    
+    // Player in sight distance
+    [SerializeField] private float fishInSightDistance = 10f;
 
     // Reference to the agent's rigid body
     private Rigidbody rb;
@@ -23,40 +25,80 @@ public class AIBehaviour : MonoBehaviour
 
     //--------------------------------------------------------------------//
 
+    private void Start() {
+        targetEulerAngles = Vector3.zero;
+    }
+    
     void Awake(){
         
         rb = GetComponent<Rigidbody>();
-        fish = GameObject.Find("tunafish");
     }
     
     // Update is called once per frame
     void Update()
     {   
-        //if (wandertime > 0){
-        //    transform.Translate(Vector3.forward * Time.deltaTime * movementSpeed );
-        //    wandertime -= Time.deltaTime; 
-        //}
-        //    
-        //else {
-        //    wandertime = Random.Range(5.0f, 5.0f);
-        //    Wander ();
-        //}
+        print($"hitWall? {hitWall}");
+        if (wanderTime > 0) {
+            if (hitWall) {
+                transform.eulerAngles = targetEulerAngles;
+                targetEulerAngles = Vector3.zero;
+                hitWall = false;
+            }
+            transform.Translate(Vector3.forward * Time.deltaTime * movementSpeed);
+            
+            wanderTime -= Time.deltaTime; 
+        }
+        else {
+            wanderTime = Random.Range(3f, 4f);
+            Wander ();
+        }
 
-        if (FishInSight()){
-            SeekAction(); 
+        //if (FishInSight()){
+        //    SeekAction();
+        //    print("im see the bitch");
+        //     
+        //}
+    }
+
+
+    private void OnTriggerEnter(Collider other) {
+        bool wall = other.CompareTag("Wall");
+        bool vertLimit = other.CompareTag("VerticalLimits");
+        if ((wall || vertLimit) && !hitWall) {
+            print($"hit wall {other.name}");
+            float eulerX = vertLimit ?
+                transform.eulerAngles.x + 180f : transform.eulerAngles.x;
+            float eulerY = wall ?
+                transform.eulerAngles.y + 180f : transform.eulerAngles.y;
+            targetEulerAngles = new Vector3(
+                eulerX,
+                transform.eulerAngles.y + 135f,
+                transform.eulerAngles.z
+            );
+            hitWall = true;
+            wanderTime = 1f;
         }
     }
 
-    void Wander (){
-        transform.eulerAngles = new Vector3 (0, Random.Range (0,360), 0);
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+        Vector3 dir = transform.TransformDirection(Vector3.forward * 200f);
+        Gizmos.DrawRay(transform.position, dir);
     }
 
+
+    void Wander () {
+        transform.eulerAngles = new Vector3 (
+            Random.Range(-20, 20), Random.Range (transform.eulerAngles.y - 20, transform.eulerAngles.y + 20), 0);
+    }
+
+    
     // Check if fish is in sight
     private bool FishInSight()
     {
         Vector3 fishPosition = fish.transform.position;
         float distance = (fishPosition - transform.position).magnitude;
-        if (distance < playerInSightDistance) return true;
+        if (distance < fishInSightDistance) return true;
         return false;
     }
     
@@ -74,6 +116,7 @@ public class AIBehaviour : MonoBehaviour
 
         // Move towards the target  at the calculated velocity
         rb.MovePosition(transform.position + vel);
+        print("im movingbitch");
 
     }
 }
